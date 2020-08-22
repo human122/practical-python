e714009f476647640e78b0908c11fc7f3d6b5d3d
human <ex@example.com>
2020-08-22 16:12:14 +0300

    added 1.9 Ex.

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -7,12 +7,15 @@ rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 months = 0
+extra_payment_start_month = 12*5
+extra_payment_end_month = extra_payment_start_month + 4*12
+extra_payment = 1000
 
 
 while principal > 0:
-    if months < 12:
-        principal = principal * (1+rate/12) - payment - 1000
-        total_paid += 1000
+    if months >= extra_payment_start_month and months < extra_payment_end_month:
+        principal = principal * (1+rate/12) - payment - extra_payment
+        total_paid += extra_payment
     else:
         principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
