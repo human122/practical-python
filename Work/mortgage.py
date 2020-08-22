4ea58a4a474f9667408dbe1ee6d63a6905f87c4a
human <ex@example.com>
2020-08-22 16:00:41 +0300

    fixed errors in 1.8 Ex.

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,15 +6,16 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
-months = 1
+months = 0
+
 
 while principal > 0:
-    principal = principal * (1+rate/12) - payment
-    total_paid = total_paid + payment
-    if months == 342:
-        print(total_paid) # 929965.6199999959
-    if months <= 12:
+    if months < 12:
+        principal = principal * (1+rate/12) - payment - 1000
         total_paid += 1000
+    else:
+        principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
     months += 1
 
 print('Total paid', total_paid)
